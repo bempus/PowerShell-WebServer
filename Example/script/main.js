6ee8ed6f95cb8000d6b1c7e7d875878376e30729
Jonathan Svahn <69847150+bempus@users.noreply.github.com>
2024-05-26 00:03:03 +0200

    Updated Server to use correct class and example to show API button

--- Example/script/main.js
+++ Example/script/main.js
@@ -1,2 +1,12 @@
 console.log("Hello from main.js");
 // This file can be placed anywhere, even in the Pages or API folders, for convienience it's placed in its own folder
+
+document.querySelector("#clickme").addEventListener("click", (e) => {
+  fetch("/api/show-message", {
+    method: "POST",
+    body: JSON.stringify({
+      message: "Hello from Webserver!",
+      title: "API",
+    }),
+  });
+});
